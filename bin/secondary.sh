8c55d0bf661b267998c962fdc61d7b15273d77d9
Lori Corbani <lori.corbani@jax.org>
2021-01-11 12:54:00 -0500

    TR13349/Genome Build 39 project

---
+++ bin/secondary.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+cd `dirname $0` 
+
+COMMON_CONFIG=../littriageload.config
+
+# 
+# Make sure the common configuration file exists and source it. 
+#
+if [ -f ${COMMON_CONFIG} ]
+then
+    . ${COMMON_CONFIG}
+else
+    echo "Missing configuration file: ${COMMON_CONFIG}"
+    exit 1
+fi
+
+LOG=${LOG_SECONDARY}
+rm -rf ${LOG}
+>>${LOG}
+
+date >> ${LOG} 2>&1
+${PYTHON} secondaryAP.py >> ${LOG} 2>&1
+date >> ${LOG} 2>&1
+
+date >> ${LOG} 2>&1
+${PYTHON} secondaryGXD.py >> ${LOG} 2>&1
+date >> ${LOG} 2>&1
+
+date >> ${LOG} 2>&1
+${PYTHON} secondaryQTL.py >> ${LOG} 2>&1
+date >> ${LOG} 2>&1
+
+#date >> ${LOG} 2>&1
+#${PYTHON} secondaryTumor.py >> ${LOG} 2>&1
+#date >> ${LOG} 2>&1
+
+date >> ${LOG} 2>&1
