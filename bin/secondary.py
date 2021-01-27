f22e1ee6551854f1ca2e59b75024509feaf7ea66
Lori Corbani <lori.corbani@jax.org>
2021-01-27 09:21:44 -0500

    TR13349/Genome Build 39 project

--- bin/secondary.py
+++ bin/secondary.py
@@ -14,7 +14,7 @@
 # GXD Criteria
 # References: relevance status = "keep", GXDstatus = "New"
 # Is Reviewed = Not Routed
-# References: relevance status = "discard", confidence > -1.0
+# References: relevance status = "discard", confidence > -1.5
 # text to search: extracted text except reference section
 # text criteria: '%embryo%' ; exclude list (vocab_key 135). (case insensitive)
 # 
@@ -356,7 +356,7 @@ def processGXD():
         and r._refs_key = v._refs_key
         and v.isCurrent = 1
         and v._relevance_key = 70594666
-        and v.confidence > -1.0
+        and v.confidence > -1.5
         and r._refs_key = s._refs_key
         and s._status_key = 71027551
         and s._group_key = 31576665
