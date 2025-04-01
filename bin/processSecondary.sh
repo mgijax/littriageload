ceba61453bb1db7e0aa635d3d5134e015ae6fd1b
Lori Corbani <lori.corbani@jax.org>
2025-04-01 07:17:34 -0400

    per Cindy: PRO post reports no longer needed

--- bin/processSecondary.sh
+++ bin/processSecondary.sh
@@ -25,9 +25,10 @@ export PYTHONPATH
 ${PYTHON} secondary.py >> ${LOG} 2>&1
 date >> ${LOG} 2>&1
 
-date >> ${LOG} 2>&1
-./secondaryReports.csh >> ${LOG} 2>&1
-date >> ${LOG} 2>&1
+# 04/02/2025 no longer needed
+#date >> ${LOG} 2>&1
+#./secondaryReports.csh >> ${LOG} 2>&1
+#date >> ${LOG} 2>&1
 
 # copy logs/gxd to /data/reports/qcreports_db/archive/gxd
 cp -r ${LOGDIR}/gxd ${QCREPORTDIR}/archive/gxd/littriage.`date '+%Y%m%d.%H%M'`
